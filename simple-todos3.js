184bd0906cc30cc0afefa8b2e0c727f8f6a1be28
iamwave007 <iamwave007@hotmail.com>
2014-12-27 07:44:17 -0500

    input

--- simple-todos3.js
+++ simple-todos3.js
@@ -1,18 +1,21 @@
 Items =new Mongo.Collection("items");
 
 if(Meteor.isClient){
-  // Template.here.helpers({
-  //   items:[
-  //     {item:"match the name 'item' is vip"},
-  //     {item:"yup"},
-  //     {item:"what if not"}
-  //     ]    
-  // });
+
   Template.here.helpers({
     items: function(){
       return Items.find({});
     }
   });
+
+  Template.body.events({
+    "submit #todo":function(e){
+      var item = e.target.item.value;
+      Items.insert({item:item});
+      e.target.item.value="";
+      return false;
+    }
+  })
 }
 
 

49991ce408d3b55b1e60eab1ed8b9f7ef174a356
iamwave007 <iamwave007@hotmail.com>
2014-12-27 07:19:47 -0500

    db

--- simple-todos3.js
+++ simple-todos3.js
@@ -1,11 +1,43 @@
+Items =new Mongo.Collection("items");
 
 if(Meteor.isClient){
+  // Template.here.helpers({
+  //   items:[
+  //     {item:"match the name 'item' is vip"},
+  //     {item:"yup"},
+  //     {item:"what if not"}
+  //     ]    
+  // });
   Template.here.helpers({
-    items:[
-      {item:"match the name 'item' is vip"},
-      {item:"yup"},
-      {item:"what if not"}
-      ]    
+    items: function(){
+      return Items.find({});
+    }
   });
-  
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
